879a54c6f15984a4efc0eff7cda69ce0fa7ec862
nazmulhossen1 <nazmulhossen539@gmail.com>
2023-12-12 23:45:11 +0600

    added project all link done

--- src/components/pages/MyProject.jsx
+++ src/components/pages/MyProject.jsx
@@ -5,32 +5,39 @@ const MyProject = () => {
         <div className="max-w-screen-xl mx-auto px-4 my-12">
             <Titile heading={'My Project'}> </Titile>
 
-            <div  className="md:flex items-center  gap-12  my-16 space-y-12 md:space-y-0" >
+            <div className="md:flex items-center  gap-12  my-16 space-y-12 md:space-y-0" >
                 <div>
                     <img className="w-4/5 md:w-full mx-auto" src="/public/images/Untitled (1).png " alt="" />
                     <div className="md:flex items-center justify-center gap-7 space-y-4 space-x-2 mt-1 ">
+                        <a href="https://smart-asset-managment.web.app/" className="outline-none btn btn-sm bg-green-600 mt-4 ">
+                            Live Link</a>
+                        <a href="https://github.com/nazmul-main/client-smart-asset-manager" className="outline-none btn btn-sm bg-green-600 mt-4 ">
+                            Client Link</a>
+                        <a href="https://github.com/nazmul-main/server-smart-asset-manager" className="outline-none btn btn-sm bg-green-600 mt-4 ">
+                            Server Link</a>
+                    </div>
+                </div>
+                <div>
+                    <img src="/public/images/Untitled (2).png" alt="" />
+                    <div className="md:flex items-center justify-center gap-7 space-y-4 space-x-2 mt-1 ">
+                        <a href="https://the-blogopolis.web.app/" className="outline-none btn btn-sm bg-green-600 mt-4 ">
+                            Live Link</a>
+                        <a href="https://github.com/nazmul-main/the-bolgopolis-client" className="outline-none btn btn-sm bg-green-600 mt-4 ">
+                            client Link</a>
+                        <a href="https://github.com/nazmul-main/the-blogopolis-server" className="outline-none btn btn-sm bg-green-600 mt-4 ">
+                            Server Link</a>
+                    </div>
+                </div>
+                <div>
+                    <img src="/public/images/Untitled (3).png" alt="" />
+                   
+                    <div className="md:flex items-center justify-center gap-7 space-y-4 space-x-2 mt-1 ">
                         <a href="https://brand-sotre.web.app/" className="outline-none btn btn-sm bg-green-600 mt-4 ">
                             Live Link</a>
-                        <button className="btn btn-sm ">Client Link</button>
-                        <button className="btn btn-sm">Server Link</button>
-                    </div>
-                </div>
-                <div>
-                    <img  src="/public/images/Untitled (2).png" alt="" />
-                    <div className="md:flex items-center justify-center gap-7 space-y-4 space-x-2 mt-1 ">
-                    <a href="https://the-blogopolis.web.app/" className="outline-none btn btn-sm bg-green-600 mt-4 ">
-                            Live Link</a>
-                        <button className="btn btn-sm ">Client Link</button>
-                        <button className="btn btn-sm">Server Link</button>
-                    </div>
-                </div>
-                <div>
-                    <img  src="/public/images/Untitled (3).png" alt="" />
-                    <div className="md:flex items-center justify-center gap-7 space-y-4 space-x-2 mt-1 ">
-                    <a href="https://the-blogopolis.web.app/" className="outline-none btn btn-sm bg-green-600 mt-4 ">
-                            Live Link</a>
-                        <button className="btn btn-sm ">Client Link</button>
-                        <button className="btn btn-sm">Server Link</button>
+                        <a href="https://github.com/nazmul-main/brand-store-client" className="outline-none btn btn-sm bg-green-600 mt-4 ">
+                            Client Link</a>
+                        <a href="https://github.com/nazmul-main/brand-store-server" className="outline-none btn btn-sm bg-green-600 mt-4 ">
+                            Server Link</a>
                     </div>
                 </div>
             </div>
